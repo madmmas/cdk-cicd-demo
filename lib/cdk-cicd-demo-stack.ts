8e432b353eb55853f04af6fa9e5175cec4d9c617
Moinuddin Masud <moinuddinmasud@gmail.com>
2025-07-02 01:45:46 -0700

    lambda attach to a apigateway in the stack

--- lib/cdk-cicd-demo-stack.ts
+++ lib/cdk-cicd-demo-stack.ts
@@ -1,11 +1,28 @@
+import path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 
 export class CdkCicdDemoStack extends cdk.Stack {
   
+  urlOutput: cdk.CfnOutput;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const handler = new lambda.Function(this, 'Handler', {
+      code: new lambda.AssetCode(path.resolve(__dirname, 'lambda')),
+      handler: 'handler.handler',
+      runtime: lambda.Runtime.PYTHON_3_9,
+    });
+
+    const gw = new apigateway.LambdaRestApi(this, 'ApiGateway', {
+      description: 'Endpoint for simple lambda powered web service',
+      handler: handler,
+    });
+
+    this.urlOutput = new cdk.CfnOutput(this, 'url', { value: gw.url });
   }
 }
