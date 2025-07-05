3d9a1df413e5eb2a8c62db064457b6731543f71c
Moinuddin Masud <moinuddinmasud@gmail.com>
2025-07-04 21:07:21 -0700

    canary codedeployment added

--- lib/cdk-cicd-demo-stack.ts
+++ lib/cdk-cicd-demo-stack.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkCicdDemoStack extends cdk.Stack {
@@ -17,9 +18,20 @@ export class CdkCicdDemoStack extends cdk.Stack {
       runtime: lambda.Runtime.PYTHON_3_12,
     });
 
+    const alias = new lambda.Alias(this, 'x', {
+      aliasName: 'Current',
+      version: handler.currentVersion,
+    });
+    
     const gw = new apigateway.LambdaRestApi(this, 'ApiGateway', {
       description: 'Endpoint for simple lambda powered web service',
-      handler: handler,
+      handler: alias,
+    });
+
+    new codedeploy.LambdaDeploymentGroup(this, 'DeploymentGroup ', {
+      alias,
+      deploymentConfig:
+        codedeploy.LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
     });
 
     this.urlOutput = new cdk.CfnOutput(this, 'url', { value: gw.url });
