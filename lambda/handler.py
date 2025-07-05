8d700224f380a66fb7d068fd035dd19867ac87ef
Moinuddin Masud <moinuddinmasud@gmail.com>
2025-07-04 19:16:18 -0700

    Test pipeline deployment

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,6 +1,7 @@
 def handler(event, context):
     # Your Lambda function logic goes here
+    # Updated to test CDK pipeline deployment
     return {
         "statusCode": 200,
-        "body": 'Hello from Lambda!',
+        "body": 'Hello from Lambda! Pipeline test successful!',
     }
