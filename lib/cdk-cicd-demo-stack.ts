8e96a1bc8e1c52e15523964bfaffb0118d85a584
Moinuddin Masud <moinuddinmasud@gmail.com>
2025-07-04 21:20:01 -0700

    cloudwatch matric alerm added

--- lib/cdk-cicd-demo-stack.ts
+++ lib/cdk-cicd-demo-stack.ts
@@ -4,6 +4,8 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+import { Duration } from 'aws-cdk-lib';
 
 export class CdkCicdDemoStack extends cdk.Stack {
   
@@ -28,10 +30,28 @@ export class CdkCicdDemoStack extends cdk.Stack {
       handler: alias,
     });
 
+    const apiGateway5xx = new cloudwatch.Metric({
+      metricName: '5XXError',
+      namespace: 'AWS/ApiGateway',
+      dimensionsMap: {
+        ApiName: gw.restApiName,
+      },
+      statistic: 'Sum',
+      period: Duration.minutes(1),
+    });
+
+    const failureAlarm = new cloudwatch.Alarm(this, 'RollbackAlarm', {
+      metric: apiGateway5xx,
+      threshold: 1,
+      evaluationPeriods: 1,
+    });
+
+    
     new codedeploy.LambdaDeploymentGroup(this, 'DeploymentGroup ', {
       alias,
       deploymentConfig:
         codedeploy.LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
+      alarms: [failureAlarm],
     });
 
     this.urlOutput = new cdk.CfnOutput(this, 'url', { value: gw.url });
